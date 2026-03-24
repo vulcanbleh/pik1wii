317c6df9a9b0332f334e86df5847a48d932e9d6f
vulcanbleh <148649099+vulcanbleh@users.noreply.github.com>
2026-03-24 06:28:34 +0000

    nw4r(1)

---
+++ include/nw4r/math.h
@@ -0,0 +1,12 @@
+#ifndef NW4R_MATH_H
+#define NW4R_MATH_H
+
+// Many thanks to OGWS and SS decomps for most of this work!
+
+#include <nw4r/math/math_arithmetic.h>
+#include <nw4r/math/math_constant.h>
+#include <nw4r/math/math_geometry.h>
+#include <nw4r/math/math_triangular.h>
+#include <nw4r/math/math_types.h>
+
+#endif
